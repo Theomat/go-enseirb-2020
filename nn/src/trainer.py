d52f567fb3375a7a99e654e80fb1116406476978
Theomat <theomatricon@gmail.com>
2020-11-29 19:56:36 +0100

    Fixed for self training

--- nn/src/trainer.py
+++ nn/src/trainer.py
@@ -1,37 +1,49 @@
 from mcts import MCTS
 from model import AlphaGoZero
 from loss import alpha_go_zero_loss
+from uniform_replay_buffer import UniformReplayBuffer
 
 import time
 
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
+from tqdm import trange
+
+import numpy as np
+
 
 class Trainer:
 
-    def __init__(self, optimizer, replay_buffer):
+    def __init__(self, replay_buffer, lr: float = 1e-3, file: str = None,
+                 episodes_per_step: int = 100, checkpoint: int = 1000):
         # autodetect best device
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         print(f"Training on {'cpu' if self.device == 'cpu' else torch.cuda.get_device_name(0)}")
 
         self.model = AlphaGoZero().to(self.device)
-        self.best = AlphaGoZero().to(self.device)
+        # self.best = AlphaGoZero().to(self.device)
+        if file:
+            self.model.load_state_dict(torch.load(file))
+            # self.best.load_state_dict(torch.load(file))
+
         self.replay_buffer = replay_buffer
-        self.optimizer = optimizer
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr, weight_decay=1e-4)
 
         self.writer = SummaryWriter()
 
         # the number of training steps between each checkpoint
-        self.checkpoint: int = 1000
+        self.checkpoint: int = checkpoint
         self._train_steps_since_last_checkpoint: int = 0
         self.sample_size: int = 2048
+
+        self.iter: int = 0
 
     def produce_episodes(self, episodes: int = 25000):
         mcts = MCTS(self.model)
         # Maybe use tqdm ?
         for _ in range(episodes):
-            self.replay_buffer.store(mcts.self_play())
+            self.replay_buffer.store(np.expand_dims(mcts.self_play(), axis=0))
 
     def _training_step(self):
         self._train_steps_since_last_checkpoint += 1
@@ -52,14 +64,22 @@ class Trainer:
         self.optimizer.step()
 
         # TODO: Also add the loss or print it so we have an update
-        # writer.add_scalars('loss', {'train_loss': running_loss / 100, 'test_los': test_loss}, n_iter)
+        running_loss = loss.item()
+        self.writer.add_scalar('train_loss', running_loss, self.iter)
+        self.iter += 1
 
     def train(self, training_steps: int):
-        for _ in range(training_steps):
+        for _ in trange(training_steps):
+            self.produce_episodes()
             self._training_step()
 
     def on_checkpoint(self):
         # TODO: keep the best NN, should we really do that ? I'm a bit lazy :P (just keep the last one, fuck it XD)
-
         # Save the best model
-        torch.save(self.best.state_dict(), f'alphago_zero_model_{int(time.time())}.pt')
+        torch.save(self.model.state_dict(), f'alphago_zero_model_{int(time.time())}.pt')
+
+
+if __name__ == "__main__":
+    buffer = UniformReplayBuffer(size=10**6)
+    trainer = Trainer(buffer, file="./model_9.pt", episodes_per_step=100)
+    trainer.train(10)
