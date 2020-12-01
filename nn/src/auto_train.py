cb7e4e11a05bf768cc1023c9ee2d308c8aa77a12
Theomat <theomatricon@gmail.com>
2020-12-01 11:52:11 +0100

    Added option to change recursion limit

--- nn/src/auto_train.py
+++ nn/src/auto_train.py
@@ -15,11 +15,15 @@ parser.add_argument('--replay-size', help='replay buffer size', default=1e5, typ
 parser.add_argument('--episodes', type=int, default=20, help="number of episodes per epoch")
 parser.add_argument('--checkpoints', default=5, type=int, help="number of epochs between checkpoints")
 parser.add_argument('--batch-size', default=1024, type=int, help="batch size")
+parser.add_argument('--recursion-limit', default=2000, type=int, help="maximum recursion depth")
+
 
 # parser.add_argument('-v', '--verbose', dest='verbose', action='store_const', const=True, default=False)
 
 parameters = parser.parse_args(sys.argv[1:])
 parameters.output = parameters.output or parameters.model
+
+sys.setrecursionlimit(parameters.recursion_limit)
 
 buffer = UniformReplayBuffer(size=parameters.replay_size)
 trainer = Trainer(buffer, file=parameters.model, episodes_per_step=parameters.episodes, lr=parameters.lr,
